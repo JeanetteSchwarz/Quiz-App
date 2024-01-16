36c0be5cc4fa95468465aa00ab6c99f6528cc15b
Jeanette Schwarz <jeanne.schwarz1306@gmail.com>
2024-01-16 22:17:14 +0100

    add darkmode

---
+++ js/profile.js
@@ -0,0 +1,7 @@
+const bodyElement = document.querySelector('[data-js="body"]');
+
+/*const darkbutton = document.querySelector('[data-js="darkmode"]');
+
+darkbutton.addEventListener("click", () => {
+  bodyElement.classList.add("switchdark");
+});*/
